626a0f6d5f395ebc00d187d0b67c54e49fc1ff3a
fbmoreira <fbirck30@gmail.com>
2022-05-06 09:31:37 -0300

    minor clarifications

--- map_analysis.py
+++ map_analysis.py
@@ -1,12 +1,19 @@
 import sys
 
 # a map is a tuple of matrix and a dict with neighbor info
-def readmap(inputfile,alignment):
+def read_map(inputfile,alignment):
 	f = open(inputfile, "r")
 	mat = []
 	for line in f:
 		mat.append(line.splitlines()[0].split(','))
 	f.close()
+	lencmp = 0
+	if len(mat[0]) == len(mat[1]):
+		lencmp = 0
+	elif len(mat[0]) < len(mat[1]):
+		lencmp = 1
+	else:
+		lencmp = 0
 	nbs = {}
 	for row in range(0,len(mat)):
 		for col in range(0,len(mat[row])):
@@ -118,6 +125,14 @@ def f4(tm_map):
 	mat = tm_map[0]
 	nbs = tm_map[1]
 	tot = 0
+	counts = {}
+	counts['Y'] = 0
+	counts['R'] = 0
+	counts['S'] = 0
+	counts['G'] = 0
+	counts['B'] = 0
+	counts['K'] = 0
+	counts['U'] = 0
 	isOneSpade = {}
 	isOneSpade['R'] = {'S','Y'}
 	isOneSpade['S'] = {'R','G'}
@@ -137,12 +152,15 @@ def f4(tm_map):
 						break
 				if not occ:
 					tot+=1
+					counts[mat[row][col]] += 1
 					pr.append(1)
 				else:
 					pr.append(0)
 			else:
 				pr.append(0)
 		print(pr)
+	print("per color infractions")
+	print(counts)
 	return tot
 
 def f5(tm_map): #peninsula
@@ -167,6 +185,72 @@ def f5(tm_map): #peninsula
 		print(pr)
 	return tot
 
+def f6(tm_map): #centrality
+	mat = tm_map[0]
+	nbs = tm_map[1]
+	counts = {}
+	counts['Y'] = 0
+	counts['R'] = 0
+	counts['S'] = 0
+	counts['G'] = 0
+	counts['B'] = 0
+	counts['K'] = 0
+	counts['U'] = 0
+	counts['I'] = 0
+	dists = {}
+	dists['Y'] = 0
+	dists['R'] = 0
+	dists['S'] = 0
+	dists['G'] = 0
+	dists['B'] = 0
+	dists['K'] = 0
+	dists['U'] = 0
+	dists['I'] = 0
+	for row in range(0,len(mat)):
+		for col in range(0,len(mat[row])):
+			v = min(row,len(mat)-row)
+			h = min(col,len(mat[row])-col)
+			dists[mat[row][col]] += h + v
+			counts[mat[row][col]] += 1
+	for k in counts:
+		print(k + ": " + str(dists[k]/counts[k]))
+	print("raw centrality")
+	print(dists)
+
+def f7(tm_map): #neighborhood
+	mat = tm_map[0]
+	nbs = tm_map[1]
+	counts = {}
+	counts['Y'] = 0
+	counts['R'] = 0
+	counts['S'] = 0
+	counts['G'] = 0
+	counts['B'] = 0
+	counts['K'] = 0
+	counts['U'] = 0
+	counts['I'] = 0
+	dists = {}
+	dists['Y'] = 0
+	dists['R'] = 0
+	dists['S'] = 0
+	dists['G'] = 0
+	dists['B'] = 0
+	dists['K'] = 0
+	dists['U'] = 0
+	dists['I'] = 0
+	for row in range(0,len(mat)):
+		for col in range(0,len(mat[row])):
+			for r,c in nbs[(row,col)]:				
+				if mat[r][c] != 'I':
+					dists[mat[row][col]] += 1
+			counts[mat[row][col]] += 1
+	for k in counts:
+		print(k + ": " + str(dists[k]/counts[k]))
+	print("raw neighborhood")
+	print(dists)
+
+
+
 if __name__ == '__main__':
 	infile = sys.argv[1]
 	a = 0
@@ -177,7 +261,7 @@ if __name__ == '__main__':
 			print("we only accept 'skewed' as a parameter so far...")
 			sys.exit()
 
-	tm_map = readmap(infile,a)
+	tm_map = read_map(infile,a)
 	print("f1 analysis")
 	print(str(f1(tm_map)) + " infractions")
 	print("f2 analysis")
@@ -187,3 +271,7 @@ if __name__ == '__main__':
 	print("f4 analysis") 
 	print(str(f4(tm_map)) + " infractions")
 	print(str(f5(tm_map)) + " isolated peninsulas")
+	print("normalized centrality")
+	f6(tm_map)
+	print("normalized neighborhood")
+	f7(tm_map)
